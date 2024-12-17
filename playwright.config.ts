3d378f70fafdeb27ccee649f547036ea86d743c7
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-12-17 09:51:08 +0200

    added UI tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices, ReporterDescription } from '@playwright/test';
 
+const reporter = [
+  ['html', { open: 'never', outputFolder: './playwright-report'}],
+  ['junit', { outputFile: './playwright-report/results.xml' }]
+] as ReporterDescription[];
+
+if (process.env.CI) {
+  reporter.push(['github']);
+}
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -21,7 +29,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
